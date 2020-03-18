2208bb8f5fa10f7e3e47a53ca92bfcc7f204b774
Daniel Ching <carterbox@users.noreply.github.com>
2020-03-18 17:26:07 -0500

    BLD: Adapt Win build directions for subpackage layout
    We should prefix the source directories in the build.bat with %SRC_DIR%
    to ensure that they are located correctly. Then we need to specify
    specifically which outputs are included in each subpackage just like for
    linux.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,29 +8,29 @@ nmake /f Makefile.vc CFG=release-dynamic RTLIBCFG=dynamic OBJDIR=output
 if errorlevel 1 exit 1
 
 :: Copy the dll's of these dependencies
-copy output\release-dynamic\%ARCH%\bin\cwebp.exe %LIBRARY_PREFIX%\bin\cwebp.exe
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\bin\cwebp.exe %LIBRARY_PREFIX%\bin\cwebp.exe
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\dwebp.exe %LIBRARY_PREFIX%\bin\dwebp.exe
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\bin\dwebp.exe %LIBRARY_PREFIX%\bin\dwebp.exe
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\libwebp.dll %LIBRARY_PREFIX%\bin\libwebp.dll
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\bin\libwebp.dll %LIBRARY_PREFIX%\bin\libwebp.dll
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\libwebpdecoder.dll %LIBRARY_PREFIX%\bin\libwebpdecoder.dll
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\bin\libwebpdecoder.dll %LIBRARY_PREFIX%\bin\libwebpdecoder.dll
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\libwebpdemux.dll %LIBRARY_PREFIX%\bin\libwebpdemux.dll
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\bin\libwebpdemux.dll %LIBRARY_PREFIX%\bin\libwebpdemux.dll
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebp_dll.lib %LIBRARY_PREFIX%\lib\libwebp.lib
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\lib\libwebp_dll.lib %LIBRARY_PREFIX%\lib\libwebp.lib
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebpdecoder_dll.lib %LIBRARY_PREFIX%\lib\libwebpdecoder.lib
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\lib\libwebpdecoder_dll.lib %LIBRARY_PREFIX%\lib\libwebpdecoder.lib
 if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebpdemux_dll.lib %LIBRARY_PREFIX%\lib\libwebpdemux.lib
+copy %SRC_DIR%\output\release-dynamic\%ARCH%\lib\libwebpdemux_dll.lib %LIBRARY_PREFIX%\lib\libwebpdemux.lib
 if errorlevel 1 exit 1
 
 :: Copy header files
 mkdir %LIBRARY_PREFIX%\include\webp\
 if errorlevel 1 exit 1
-copy src\webp\decode.h %LIBRARY_PREFIX%\include\webp\
+copy %SRC_DIR%\src\webp\decode.h %LIBRARY_PREFIX%\include\webp\
 if errorlevel 1 exit 1
-copy src\webp\encode.h %LIBRARY_PREFIX%\include\webp\
+copy %SRC_DIR%\src\webp\encode.h %LIBRARY_PREFIX%\include\webp\
 if errorlevel 1 exit 1
-copy src\webp\types.h %LIBRARY_PREFIX%\include\webp\
+copy %SRC_DIR%\src\webp\types.h %LIBRARY_PREFIX%\include\webp\
 if errorlevel 1 exit 1
